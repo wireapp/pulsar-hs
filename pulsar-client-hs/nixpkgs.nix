1899574b3b3edab60dca5c655f9abf97e6397d56
Sven Tennie <sven.tennie@wire.com>
2025-10-22 11:02:31 +0200

    Update nix-env to 25.05
    This includes using HLS from nixpkgs and adjusting the C++ build to use
    C++ 17.
    
    C++ std fixup

--- pulsar-client-hs/nixpkgs.nix
+++ pulsar-client-hs/nixpkgs.nix
@@ -1,1 +1,1 @@
-import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/b5182c214fac1e6db9f28ed8a7cfc2d0c255c763.tar.gz"; })
+import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/a493e93b4a259cd9fea8073f89a7ed9b1c5a1da2.tar.gz"; })
