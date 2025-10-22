ae9a401fca437abb7835a9b3ac685798b734e9d2
Sven Tennie <sven.tennie@wire.com>
2025-10-22 16:31:04 +0200

    Provide all dependencies in shell.nix
    Some library dependencies for pulsar-client-hs were missing. This setup
    provides all of them by slightly changing the structure.

--- pulsar-client-hs/shell.nix
+++ pulsar-client-hs/shell.nix
@@ -1,15 +1,23 @@
 { nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc98" }:
 let
   pkgs = import ./default.nix { inherit nixpkgs compiler; };
+  haskellPackages = nixpkgs.haskellPackages.override {
+    overrides = self: super: {
+      pulsar-client-hs = pkgs.pulsar-client-hs;
+    };
+  };
 in
-  nixpkgs.mkShell {
-    buildInputs = [
-      pkgs.pulsar-client-cpp
-      nixpkgs.haskellPackages.ghcid
-      nixpkgs.haskell.compiler.${compiler}
-      nixpkgs.haskellPackages.c2hsc
-      nixpkgs.haskellPackages.cabal-install
-      nixpkgs.haskellPackages.hsc2hs
-      (nixpkgs.haskell-language-server.override { supportedGhcVersions = [ (nixpkgs.lib.removePrefix "ghc" compiler) ]; })
-    ];
-  }
+haskellPackages.shellFor {
+  packages = p: [ p.pulsar-client-hs ];
+
+  buildInputs = [
+    haskellPackages.ghcid
+    nixpkgs.haskell.compiler.${compiler}
+    haskellPackages.c2hsc
+    haskellPackages.cabal-install
+    haskellPackages.hsc2hs
+    (nixpkgs.haskell-language-server.override {
+      supportedGhcVersions = [ (nixpkgs.lib.removePrefix "ghc" compiler) ];
+    })
+  ];
+}
