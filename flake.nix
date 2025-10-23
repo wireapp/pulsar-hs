d6428efcafc6115e0d38fa4fa46879f58747b6d3
Sven Tennie <sven.tennie@wire.com>
2025-10-23 15:17:49 +0200

    Create dedicated devShells for packages
    Otherwise, we don't get local package dependencies injected into the
    ghc-pkg list.

--- flake.nix
+++ flake.nix
@@ -6,58 +6,123 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        compiler = "ghc98";
-        haskellPackages = pkgs.haskell.packages.${compiler};
-
-        # Native C++ library
-        pulsar-client-cpp = pkgs.callPackage ./pulsar-client-hs/native {
-          inherit (pkgs) stdenv lib pkgs boost177 cmake curl jsoncpp
-                         log4cxx openssl pkgconf protobuf_26 python3
-                         snappy zlib zstd fetchurl;
+        packageOverlay = final: prev: {
+          # Native C++ library. The original pulsar package is marked as
+          # insecure, so we simply override it.
+          pulsar-client-cpp = prev.callPackage ./pulsar-client-hs/native {
+            inherit (pkgs)
+              stdenv
+              lib
+              pkgs
+              boost177
+              cmake
+              curl
+              jsoncpp
+              log4cxx
+              openssl
+              pkgconf
+              protobuf_26
+              python3
+              snappy
+              zlib
+              zstd
+              fetchurl
+              ;
+          };
         };
 
-      in
-      rec
-      {
-        packages.pulsar-client-cpp = pulsar-client-cpp;
+        pkgs = nixpkgs.legacyPackages.${system}.extend packageOverlay;
 
-        packages.pulsar-client-hs =
-          haskellPackages.callCabal2nix "pulsar-client-hs" ./pulsar-client-hs {
-            pulsar = pulsar-client-cpp;
+        compiler = "ghc98";
+
+        haskellPackages = pkgs.haskell.packages.${compiler}.override {
+          overrides = self: super: {
+            pulsar-client-hs = self.callCabal2nix "pulsar-client-hs" ./pulsar-client-hs {
+              pulsar = pkgs.pulsar-client-cpp;
+            };
+
+            pulsar-admin = pkgs.haskell.lib.dontCheck (self.callCabal2nix "pulsar-admin" ./pulsar-admin { });
+
+            pulsar-client-hs-examples =
+              self.callCabal2nix "pulsar-client-hs-examples" ./pulsar-client-hs/examples
+                { };
           };
+        };
 
-        packages.pulsar-admin =
-          pkgs.haskell.lib.dontCheck (
-            haskellPackages.callCabal2nix "pulsar-admin" ./pulsar-admin {}
-          );
+        localHaskellPackages = {
+          pulsar-client-hs = haskellPackages.pulsar-client-hs;
+          pulsar-admin = haskellPackages.pulsar-admin;
+          pulsar-client-hs-examples = haskellPackages.pulsar-client-hs-examples;
+        };
 
-        packages.pulsar-client-hs-examples =
-          haskellPackages.callCabal2nix "pulsar-client-hs-examples" ./pulsar-client-hs/examples {
-            pulsar-client-hs = packages.pulsar-client-hs;
-          };
-
-        defaultPackage = pkgs.linkFarmFromDrvs "all-pulsar-hs" [
-          packages.pulsar-client-hs
-          packages.pulsar-admin
-          packages.pulsar-client-hs-examples
-        ];
-
-        devShell = pkgs.mkShell {
-          buildInputs = with haskellPackages; [
+        devTools =
+          with haskellPackages;
+          [
             haskell-language-server
             ghcid
             cabal-install
             hlint
             c2hsc
             hsc2hs
-          ] ++ [
+          ]
+          ++ [
             pkgs.haskell.compiler.${compiler}
           ];
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+
+        # Haskell packages in `packages` are not registered as packages in GHC.
+        # Thus, we have to create one dedicated env per local Haskell package
+        # to provide all of its dependecies.
+        mkSingleDevShell =
+          pkgName:
+          haskellPackages.shellFor {
+            packages = p: [ p.${pkgName} ];
+            buildInputs = devTools;
+          };
+
+        haskellPackageNames = builtins.attrNames localHaskellPackages;
+
+        mkPackageAttrs =
+          pkgNames:
+          builtins.listToAttrs (
+            map
+              (pkgName: {
+                name = pkgName;
+                value = haskellPackages.${pkgName};
+              })
+              pkgNames
+          );
+      in
+      rec {
+        packages = {
+          pulsar-client-cpp = pkgs.pulsar-client-cpp;
+        }
+        // mkPackageAttrs haskellPackageNames;
+
+        defaultPackage = pkgs.linkFarmFromDrvs "all-pulsar-hs" (builtins.attrValues localHaskellPackages);
+
+        devShells = {
+          default = haskellPackages.shellFor {
+            packages = p: builtins.attrValues localHaskellPackages;
+            buildInputs = devTools;
+          };
+        }
+        // builtins.listToAttrs (
+          map
+            (pkgName: {
+              name = pkgName;
+              value = mkSingleDevShell pkgName;
+            })
+            haskellPackageNames
+        );
+      }
+    );
 }
