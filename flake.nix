c5e8520b0c3efbd022659ceb642fb73e903bd933
Sven Tennie <sven.tennie@wire.com>
2025-11-13 18:24:45 +0100

    Extract overlay.nix
    Provide the oberlay for old-scholl nixpkgs.

--- flake.nix
+++ flake.nix
@@ -14,36 +14,7 @@
     }:
     let
       # Why do we need to `let`s? The overlay output should be system (platform) agnostic.
-      haskellOverride = final: prev: self: super: {
-        pulsar-client-hs = super.callCabal2nix "pulsar-client-hs" ./pulsar-client-hs {
-          pulsar = final.libpulsar;
-        };
-        pulsar-admin = prev.haskell.lib.dontCheck (super.callCabal2nix "pulsar-admin" ./pulsar-admin { });
-      };
-
-      packageOverlay = final: prev: {
-        libpulsar = prev.libpulsar.overrideAttrs (old: {
-          patches = (old.patches or [ ]) ++ [
-            ./nix/add-stdbool-table-view.patch
-          ];
-        });
-
-        haskellPackages = prev.haskellPackages.override { overrides = haskellOverride final prev; };
-
-        haskell = prev.haskell // {
-          packages = prev.haskell.packages //
-            builtins.listToAttrs (
-              map
-                (compilerName: {
-                  name = compilerName;
-                  value = prev.haskell.packages.${compilerName}.override {
-                    overrides = haskellOverride final prev;
-                  };
-                })
-                (builtins.attrNames prev.haskell.packages)
-            );
-        };
-      };
+      packageOverlay = import ./nix/overlay.nix;
     in
     flake-utils.lib.eachDefaultSystem
       (
